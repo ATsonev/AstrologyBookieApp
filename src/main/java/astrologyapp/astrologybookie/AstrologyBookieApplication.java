ce883fffc897c0d3e1a5a342a8ba4caff9d7577f
ATsonev <alexconev1@gmail.com>
2024-09-19 14:26:30 +0300

    Initial commit

---
+++ src/main/java/astrologyapp/astrologybookie/AstrologyBookieApplication.java
@@ -0,0 +1,13 @@
+package astrologyapp.astrologybookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrologyBookieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstrologyBookieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/astrologyapp/astrologybookie/AstrologyBookieApplicationTests.java
@@ -0,0 +1,13 @@
+package astrologyapp.astrologybookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstrologyBookieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
